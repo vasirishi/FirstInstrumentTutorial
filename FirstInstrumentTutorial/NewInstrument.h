76a50827a20b6d18dfc47f2f8af1d53fc1f07767
Nicholas Arner <nicholasarner@gmail.com>
2015-02-03 19:04:58 -0500

    Created instrument file

---
+++ FirstInstrumentTutorial/NewInstrument.h
@@ -0,0 +1,13 @@
+//
+//  NewInstrument.h
+//  FirstInstrumentTutorial
+//
+//  Created by Nicholas Arner on 2/3/15.
+//  Copyright (c) 2015 Nicholas Arner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewInstrument : UIViewController
+
+@end
