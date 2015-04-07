a8fdd55a301d0d038cc5371431f1deee8570c2fa
Nicholas Arner <nicholasarner@gmail.com>
2015-04-07 18:51:36 -0400

    Created new project

---
+++ FirstInstrumentTutorial/FirstInstrumentTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstInstrumentTutorial
+//
+//  Created by Nicholas Arner on 4/7/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstInstrumentTutorial/FirstInstrumentTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstInstrumentTutorial
+//
+//  Created by Nicholas Arner on 4/7/15.
+//  Copyright (c) 2015 AudioKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
